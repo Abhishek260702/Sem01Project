7898b99a8859cf0d37189f82e24e99011f001acf
abhishek <abhisheknegi260702@gmail.com>
2025-06-30 21:03:24 +0100

    Started FoodListing with basic state for foods and form inputs

---
+++ menu/src/components/FoodListing.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import './FoodListing.css';
+
+const FoodListing = () => {
+
+    const [food, setFood] = useState([]);
+    const [formData ,setFormData] =useState({
+        foodName : "",
+        price: "" ,
+        desc: "",
+    })
+    
+}
