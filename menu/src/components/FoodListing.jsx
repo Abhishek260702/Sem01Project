a9d3df674d185a7fddcd25c714d5d6211af2a9aa
abhishek <abhisheknegi260702@gmail.com>
2025-07-01 11:57:15 +0100

    created a food menu where you can see items, remove them, and add new ones.

--- menu/src/components/FoodListing.jsx
+++ menu/src/components/FoodListing.jsx
@@ -4,12 +4,12 @@ import './FoodListing.css';
 const FoodListing = () => {
 
     const [food, setFood] = useState([]);
-    const [formData ,setFormData] =useState({
-        foodName : "",
-        price: "" ,
+    const [formData, setFormData] = useState({
+        foodName: "",
+        price: "",
         desc: "",
     })
- const menu = async () => {
+    const menu = async () => {
         try {
             const data = await fetch("http://localhost:3000/menu");
             const response = await data.json();
@@ -21,44 +21,67 @@ const FoodListing = () => {
 
         }
     }
-     useEffect(() => {
+    useEffect(() => {
         menu();
     }, [])
 
- const handleOnClick = async () =>{
-    if(!formData.foodName || !formData.price   ){
-        return alert("food name or price not entered")
+    const handleOnClick = async () => {
+        if (!formData.foodName || !formData.price) {
+            return alert("food name or price not entered")
+        }
+        try {
+            const res = await fetch("https://localhost:3000/newfood", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },//teell type is json only  
+                body: JSON.stringify(formData) //convert to string because http accepts string only
+            });
+            const result = await res.json();
+            console.log("successfully sent", result);
+            alert("food added !!")
+
+            //reset form
+            setFormData({ foodName: "", price: "", desc: "" });
+
+        }
+        catch (err) {
+            console.log("error in adding food")
+        }
     }
-    try{
-        const res = await fetch("https://localhost:3000/newfood",{
-          method : "POST",
-          headers : {"Content-Type":"application/json"},//teell type is json only  
-        body:JSON.stringify(formData) //convert to string because http accepts string only
-        });
-        const result = await res.json();
-        console.log("successfully sent" , result);
-        alert("food added !!")
-
-        //reset form
-        setFormData({foodName :"", price :"", desc:""});
-
+}
+const handleDelete = async (id) => {
+    try {
+        const res = await fetch(`http://localhost:3000/menu/${id}`, {
+            method: "DELETE"
+        })
+        alert("Item Deleted")
     }
-    catch(err){
-  console.log("error in adding food")
+    catch (err) {
+        console.log(err)
     }
-  }
-}
-const handleDelete = async(id) =>{
-try{
-    const res = await fetch(`http://localhost:3000/menu/${id}`,{
-       method : "DELETE" 
-    })
-    alert("Item Deleted")
-}
-catch(err){
-    console.log(err)
-}
 
+    return (
+        <>
+            <div className="menu-container">
+                <div className="menu-title"> Our Authentic Dishes
+                    <div className="menu-grid">
+                        {food.map((item, index) => (
+                            <div key={index} className='menu-card'>
+                                <h2 className='food-name'>{item.foodName}</h2>
+                                <p className='food-price'>Price:{item.price}</p>
+                                <p className='food-desc'>description:  {item.desc}</p>
+                                <button onClick={ ()=> handleDelete(item._id)}>Remove from Menu</button>
+                                
+                            </div>
+                        ))}
+                    </div>
+                    <div className="form-Fields">
+                        <input type="text" placeholder='Enter Food Name' value ={formData.foodName} onChange={(e) => setFormData({...formData , foodName : e.target.value})} />
+                        <input type="text" placeholder='Enter Food Price' value ={formData.price} onChange={(e)=>setFormData({...formData , price : e.target.value})} />
+                        <input type="text" placeholder='Enter Food Desc' value ={formData.desc} onChange={(e)=>setFormData({...formData , desc : e.target.value})} /> 
+                        <button onClick={handleOnClick}>Add Item</button> 
+                    </div>
+                 </div>
+             </div>
+        </>
+    )
 }
-
-
