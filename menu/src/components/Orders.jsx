96173882cd3e0d0c0ab77e2dbbf3d16976bdcd4e
abhishek <abhisheknegi260702@gmail.com>
2025-07-03 10:11:39 +0100

    Setup basic order form and connected to backend

--- menu/src/components/Orders.jsx
+++ menu/src/components/Orders.jsx
@@ -19,5 +19,35 @@ const Orders = () => {
         const { name, value } = e.target;    //destr
         setCurrentOrder({ ...currentOrder, [name]: value });      //adding [name] : value => in currentOrder
     }
+const handleSubmit = async () => {
+        const newOrder = {   //here  giving the obj by bundling all string together
+            foodName: currentOrder.foodName,
+            quantity: parseInt(currentOrder.quantity),
+            price: parseInt(currentOrder.price)
+        };
+        console.log("newOrder", newOrder)
+        setOrderInfo([...orderInfo, newOrder]);
+        console.log("orderIndo :", orderInfo);
+        console.log("currentOrder :", currentOrder);
+        setCurrentOrder({ foodName: "", quantity: "", price: "" });
 
+        try {
+            const res = await axios.post("http://localhost:3000/order", [currentOrder]);
+            console.log(res);
+        }
+        catch (err) {
+            console.log(err);
+
+        }
+    }
+
+    const fetchOrders = async () => {
+        const res = await axios.get("http://localhost:3000/showorder");
+        console.log("given orders", res.data)
+        setGivenOrders(res.data);
+    }
+
+    useEffect(() => {
+        fetchOrders();
+    }, [])
 }
