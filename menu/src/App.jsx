515ce822dd48ad556641f8509e333efd9f0111ee
abhishek <abhisheknegi260702@gmail.com>
2025-07-06 00:09:41 +0100

    Set up routing and added main pages to the app

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,1 +1,34 @@
 
+import './App.css'
+import About from './components/About';
+import Career from './components/Career';
+import FoodListing from './components/FoodListing'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Orders from './components/Orders'
+import {Routes,Route} from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/order' element={<Orders/>} />
+      <Route path='/addfood' element={<FoodListing/>} />
+       <Route path='/about' element={<About/>} />
+        <Route path='/career' element={<Career/>} />
+    </Routes>
+  
+  
+     
+    
+  
+    </>
+  )
+}
+
+export default App
+
