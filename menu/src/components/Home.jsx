1f0c12885f4da5285dbc6681fe09ff3c0ac40129
abhishek <abhisheknegi260702@gmail.com>
2025-07-06 00:47:31 +0100

    Build Home page with  basic layout and styles

--- menu/src/components/Home.jsx
+++ menu/src/components/Home.jsx
@@ -1,11 +1,62 @@
+
+import React from 'react';
+
 import './Home.css';
 
 const Home = () => {
-    return(
-        <>
-       <h1>Homepage</h1>
-        </>
-    )
+  return (
+    <>
+      <header className="main-header">
+        <div className="logo">Hotel <span>Solitaire</span></div>
+        <p className="tagline">Hotel Solitaire is a refined luxury hotel designed to offer guests an exceptional experience of comfort, 
+            elegance, and personalized service. Nestled in the heart of the city, Solitaire combines modern amenities with warm hospitality,
+             making it an ideal destination for both leisure travelers and business guests.</p>
+      </header>
+
+      <section className="hero">
+        <div className="hero-text">
+          <h1>Welcome to Hotel Solitaire</h1>
+          <p>Experience comfort, elegance, and warmth</p>
+          
+        </div>
+      </section>
+
+      <section className="about" id="about">
+        <h2>About Hotel Solitaire</h2>
+        <p>
+          Located in the heart of the city, Hotel Solitaire brings you luxurious rooms,
+          gourmet dining, and unparalleled hospitality. Whether it's a family vacation or a business trip,
+          we promise a remarkable stay.
+        </p>
+      </section>
+
+      <section className="features" id="features">
+        <h2>Our Highlights</h2>
+        <div className="feature-cards">
+          <div className="card">
+            <h3>4 Star</h3>
+            <p>Four-Star Comfort, Five-Star Service</p>
+          </div>
+          <div className="card">
+            <h3>Fine Dining</h3>
+            <p>Multi-cuisine dishes curated by top chefs in our in-house restaurant.</p>
+          </div>
+          <div className="card">
+            <h3>Exclusive Spa</h3>
+            <p>Unwind and refresh with our relaxing wellness facilities.</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="contact" id="contact">
+        <h2>Contact Us</h2>
+        <p>Email: info@hotelsolitaire.com | Phone: +11111111111</p>
+        <p>Haridwar Bypass Rd,Dehradun, Uttarakhand 248001, India</p>
+      </section>
+
+      
+    </>
+  );
 }
 
-export default Home
+export default Home;
